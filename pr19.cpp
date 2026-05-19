422e62f8ec628bcf0b276939a6161c3146fe0cab
ElizaBeth <167125218+LikaLi1@users.noreply.github.com>
2026-05-19 09:14:23 +0300

    pr19.cpp

--- pr19.cpp
+++ pr19.cpp
@@ -448,3 +448,80 @@ int main()
 
 	return 0;
 }
+
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+class Prototype
+{
+public:
+	virtual Prototype* clone() const = 0;
+	virtual ~Prototype() {}
+};
+
+class Enemy : public Prototype
+{
+private:
+	string name;
+	int level;
+
+public:
+	Enemy(const string& n, int l) : name(n), level(l) {}
+
+	Prototype* clone() const override
+	{
+		return new Enemy(*this);
+	}
+
+	void printInfo() const
+	{
+		cout << "Enemy: " << name << ", Level: " << level << endl;
+	}
+};
+
+class EnemyFactory
+{
+private:
+	map<string, Prototype*> prototypes;
+
+public:
+	void addPrototype(const string& key, Prototype* prototype)
+	{
+		prototypes[key] = prototype;
+	}
+
+	Enemy* create(const string& key) 
+	{
+		auto it = prototypes.find(key);
+		if (it != prototypes.end());
+		{
+			return dynamic_cast<Enemy*>(it->second->clone());
+		}
+		return nullptr;
+	}
+};
+
+int main()
+{
+	EnemyFactory factory;
+
+	factory.addPrototype("zombie", new Enemy("Zombie", 1));
+	factory.addPrototype("skeleton", new Enemy("Skeleton", 2));
+	factory.addPrototype("mage", new Enemy("Mage", 5));
+
+	Enemy* e1 = factory.create("zombie");
+	Enemy* e2 = factory.create("skeleton");
+	Enemy* e3 = factory.create("mage");
+
+	e1->printInfo();
+	e2->printInfo();
+	e3->printInfo();
+
+	delete e1;
+	delete e2;
+	delete e3;
+
+	return 0;
+}
